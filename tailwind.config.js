06097fd2f0fde68ec425b1a3470f302638a1f66d
YannyKyprianou <yannykyps@hotmail.com>
2021-03-30 19:26:32 +0100

    added css grid col 2 to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  // options: {
-  // safelist: ['sm:grid-cols-2', 'sm:grid-cols-4' ],
-  // },
+  options: {
+  safelist: ['sm:grid-cols-2', 'sm:grid-cols-4' ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},  
